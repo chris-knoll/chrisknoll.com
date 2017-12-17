82f48cbe8e6deb3c782d3ba0ab4b19f40f5b2ebb
Chris Knoll <cknoll3@gmail.com>
2017-12-16 22:02:25 -0800

    Add master template.

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Welcome to my site")
+	return render(request, 'main/index.html')
