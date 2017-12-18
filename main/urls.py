3251cdb786043057e2c3ef362b31b01c4eaa020f
Chris Knoll <cknoll3@gmail.com>
2017-12-17 17:17:00 -0800

    Add templates and routes for main pages.

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('resume', views.resume, name='resume'),
+    path('projects', views.projects, name='projects'),
+    path('contact', views.contact, name='contact'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-	return render(request, 'main/index.html')
+def home(request):
+	return render(request, 'main/home.html')
+
+def resume(request):
+	return render(request, 'main/resume.html')
+
+def projects(request):
+	return render(request, 'main/projects.html')
+
+def contact(request):
+	return render(request, 'main/contact.html')
